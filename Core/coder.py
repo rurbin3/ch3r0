19fe5c71fea35c6c7a6d3c3ee496cf3586e19659
Cyberbyte8869 <66154594+Cyberbyte8869@users.noreply.github.com>
2020-08-01 14:37:18 +0530

    Create coder.py

---
+++ Core/coder.py
@@ -0,0 +1,11 @@
+import time
+import datetime
+from termcolor import colored, cprint
+
+
+def Gostauto():
+	print("\033[92mVersion: " + colored("\033[91m1.0.4"))
+	print("\033[92mCoder: " + colored("\033[91mcyberbyte"))
+	print("\033[92mModules available: " + colored("\033[91m15"))
+	print("\033[92mGithub: " + colored("\033[91mhttps://github.com/Cyberbyte8869"))
+	print("\033[92mtelegram: " + colored("\033[91mhttps://t.me/cyberbyte"))
