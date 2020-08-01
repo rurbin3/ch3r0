04dcfc235a4ef0aa9805de26979376014a187ce0
Cyberbyte8869 <66154594+Cyberbyte8869@users.noreply.github.com>
2020-08-01 15:16:46 +0530

    Update and rename install.py to install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+apt update
+apt install python-tk
+apt install dirb -y
+apt install sslstrip -y
+apt install bettercap -y
+apt install sslscan -y
+apt install crunch -y
+apt install tcpdump -y
+apt install macchanger -y
+apt install wafw00f -y
+apt install medusa -y
+cd /root && git clone https://github.com/thelinuxchoice/shellphish
+cd /root && git clone https://github.com/1N3/BruteX
+cd /root/BruteX && ./install.sh
+pip22 install mechanize
+pip2 install json
+pip2 install whois
+pip2 install python-whois
+pip2 install requests
+pip2 install bs4
+pip2 install tkinter
+pip2 install requests[socks]
+pip2 install urlparse
+pip2 install cookielib
+pip2 install scapy
+pip2 install datetime
+pip2 install argparse 
+pip2 install re
+pip2 install threading
+pip2 install urllib2
+pip2 install modules
+pip2 install builtwith
+pip2 install smtplib
+pip2 install whois
+pip2 install builtwith
+pip2 install colorama
+pip2 install dnspython
+pip2 install shodan
+apt install python-socks -y
+apt install nmap -y
+apt install php -y
+apt install perl -y
+apt install hashcat -y
+apt install nc -y
+apt install neofetch -y
+apt install cupp -y
+gem install lolcat
+cd /root/ && git clone https://github.com/Und3rf10w/kali-anonsurf")
+cd /root/kali-anonsurf && ./installer.sh")
+echo " Successfully Installed Some Modules "
+	
+
